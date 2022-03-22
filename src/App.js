b9eb151a6d5852c1d1072352bfb7ae4ed676995d
PraveenPallagani <praveenpallagani999@gmail.com>
2022-03-22 10:04:14 +0530

    part4 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Part1 from './components/Part1';
 import Part2 from './components/Part2';
 import Part3 from './components/Part3';
+import Part4 from './components/Part4';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Part1/>
       <Part2/>
       <Part3/>
+      <Part4/>
     </>
   );
 }
